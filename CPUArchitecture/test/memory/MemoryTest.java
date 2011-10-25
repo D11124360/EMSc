263bb6928f431c3558d0fa69c19d4c6847f4b635
W Vajira Kuruppuarachchi <arijav@gmail.com>
2011-10-25 07:08:20 +0100

    Create tests

---
+++ CPUArchitecture/test/memory/MemoryTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package memory;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author vajira
+ *
+ */
+public class MemoryTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
