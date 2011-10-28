9b86d7756e1e5fc355f8618f9b1fcfdecae62ba5
W Vajira Kuruppuarachchi <arijav@gmail.com>
2011-10-28 06:53:44 +0100

    new testcases added

--- SDT/test/stringcalculator/TestStringCal.java
+++ SDT/test/stringcalculator/TestStringCal.java
@@ -43,6 +43,34 @@ public class TestStringCal {
 		assertEquals(6,x);
 	}
 	
-
+	@Test
+	public void commaWithNewLine(){
+		int x = StringCal.add("1,\n2");
+		assertEquals(6,x);
+	}
+	
+	@Test
+	public void changeTheDelimiter(){
+		int x = StringCal.add("//;\n1;2");
+		assertEquals(3,x);
+	}
+	
+	@Test
+	public void checkNewDelimiter(){
+		int x = StringCal.add("4;1;2");
+		assertEquals(7,x);
+	}
+	
+	@Test
+	public void throwExceptionForNegativeNumbers(){
+		
+		try {
+			int x = StringCal.add("-4;1;2");
+			fail("Exception expected");
+		} catch (RuntimeException e) {
+			//
+			
+		}
+	}
 	
 }
